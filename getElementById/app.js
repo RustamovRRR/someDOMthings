e239e180b9dec928e450d152b536a4e7054e4942
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-26 00:37:21 +0400

    simple examples of getelementbyid

---
+++ getElementById/app.js
@@ -0,0 +1,15 @@
+// getElementById
+
+const title = document.getElementById("title");
+title.style.color = "orange";
+title.style.fontSize = "4rem";
+title.style.textAlign = "center";
+title.style.background = "wheat";
+title.style.border = "2px solid grey";
+title.style.margin = "2rem 6rem";
+
+const btn = document.getElementById("btn");
+btn.style.display = "block";
+btn.style.margin = "0 auto";
+btn.style.padding = "0.5rem 2rem";
+btn.style.fontSize = "2rem";
