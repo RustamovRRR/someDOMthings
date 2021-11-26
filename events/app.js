baa08cf26ca03634db273ed3ebd7d9463a8162d5
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-27 01:30:06 +0400

    adding some functionality to click event

--- events/app.js
+++ events/app.js
@@ -8,10 +8,11 @@ const heading = document.querySelector("h1");
 
 // using addEventListener() method
 btn.addEventListener("click", function () {
-  if (!heading.classList.contains("red")) {
-    heading.classList.add("red");
-  } else {
+  let hasClass = heading.classList.contains("red");
+  if (hasClass) {
     heading.classList.remove("red");
+  } else {
+    heading.classList.add("red");
   }
 });
 // abouve with used addEventListener with click event
