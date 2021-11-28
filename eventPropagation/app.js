420230596156db0188e67d7509d9c12ba836a95e
RustamovRRR <rustamrrustamov@gmail.com>
2021-11-28 18:01:56 +0400

    understanding event propagation, bubbling, and capture

---
+++ eventPropagation/app.js
@@ -0,0 +1,24 @@
+// always select dynamic elements
+// event propagation - order the events fired
+// event bubbling - clicked element first then bubbles up -- default
+// event capturing - fires at the root and fires until reaches target
+
+const container = document.querySelector(".container");
+const list = document.querySelector(".list-items");
+
+function showBubbling(e) {
+  console.log(e.currentTarget);
+  console.log(e.target);
+  if (e.target.classList.contains("link")) {
+    console.log("contains link");
+  }
+}
+
+function stopPropagation(e) {
+  e.stopPropagation();
+}
+
+container.addEventListener("click", stopPropagation, { capture: true });
+list.addEventListener("click", showBubbling, { capture: true });
+document.addEventListener("click", showBubbling, { capture: true });
+window.addEventListener("click", showBubbling, { capture: true });
